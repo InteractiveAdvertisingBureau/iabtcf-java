9478ac17ff8d2a2483930002ccefb08bad4235ec
Paulo Miranda <paulo.miranda@inloco.com.br>
2020-02-04 17:56:22 -0300

    feat(BooleanEncoder): treat invalid input

--- src/main/java/com/iabtcf/encoder/field/BooleanEncoder.java
+++ src/main/java/com/iabtcf/encoder/field/BooleanEncoder.java
@@ -1,8 +1,12 @@
 package com.iabtcf.encoder.field;
 
 import com.iabtcf.encoder.BaseEncoder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class BooleanEncoder implements BaseEncoder<Boolean> {
+    private static final String FALSE = "0";
+    private static final String TRUE = "1";
 //    public final String encode(Boolean value) {
 //        return value + "";
 //    }
@@ -11,10 +15,25 @@ public class BooleanEncoder implements BaseEncoder<Boolean> {
     }
 
     private static final BooleanEncoder instance = new BooleanEncoder();
+
     public static BooleanEncoder getInstance() {
         return instance;
     }
-    public final Boolean decode(String value){
-        return value.equals("1");
+
+    private static final Logger logger = LogManager.getLogger(IntEncoder.class);
+
+    public final Boolean decode(String value) {
+        try {
+            if (value.equals(TRUE)) {
+                return true;
+            }
+            if (value.equals(FALSE)) {
+                return false;
+            }
+            return null;
+        } catch (Exception e) {
+            logger.error("BooleanEncoder's decoding failed:" + e.getMessage());
+        }
+        return null;
     }
 }
