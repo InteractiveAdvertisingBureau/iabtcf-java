f44f0d2f7eac661983775caa1d3f8ec4958a089c
Sleiman Jneidi <sleiman.jneidi@quantcast.com>
2020-02-19 13:16:41 +0000

    cleanup

--- src/main/java/com/iabtcf/v2/RestrictionType.java
+++ src/main/java/com/iabtcf/v2/RestrictionType.java
@@ -3,7 +3,8 @@ package com.iabtcf.v2;
 public enum RestrictionType {
     NOT_ALLOWED,
     REQUIRE_CONSENT,
-    REQUIRE_LEGITIMATE_INTEREST;
+    REQUIRE_LEGITIMATE_INTEREST,
+    UNDEFINED;
 
     public static RestrictionType fromId(int id) {
         switch (id) {
@@ -11,6 +12,8 @@ public enum RestrictionType {
                 return REQUIRE_CONSENT;
             case 2:
                 return REQUIRE_LEGITIMATE_INTEREST;
+            case 3:
+                return UNDEFINED;
             default:
                 return NOT_ALLOWED;
         }
