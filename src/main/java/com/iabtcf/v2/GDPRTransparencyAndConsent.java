178d9224bc57d8992e8271ba53ecc66b93555dbf
White, Evan <evanwht1@gmail.com>
2020-03-05 13:31:02 -0600

    segment based interfaces

---
+++ src/main/java/com/iabtcf/v2/GDPRTransparencyAndConsent.java
@@ -0,0 +1,13 @@
+package com.iabtcf.v2;
+
+/**
+ * @author evanwht1
+ */
+public interface GDPRTransparencyAndConsent {
+
+    CoreString getCoreString();
+
+    OutOfBandConsent getOutOfBandSignals();
+
+    PublisherTC getPublisherPurposesTC();
+}
